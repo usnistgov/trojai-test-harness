642c4b6b3956bc3b690b874decf3d007ac792da7
Michael Majurski <michael.majurski@nist.gov>
2020-07-24 18:29:59 -0400

    added nas cron job

---
+++ admin_scripts/cron_job_nas_usage.sh
@@ -0,0 +1,3 @@
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+@daily trojai cd /home/trojai && /home/trojai/test-env/bin/python3 -u /mnt/isgnas/project/ai/trojai/trojai-test-harness/admin_scripts/check_trojainas_usage.py
