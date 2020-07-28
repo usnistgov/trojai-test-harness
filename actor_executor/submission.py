c227b0de04c4994f01d471f4eb4ae5b9b5517ac1
Tim Blattner <timothy.blattner@nist.gov>
2020-07-27 21:25:13 -0400

    - Fixed bug with selecting submission

--- actor_executor/submission.py
+++ actor_executor/submission.py
@@ -174,9 +174,9 @@ class SubmissionManager(object):
 
             if best_submission is not None:
                 if execute_team_name is not None and execute_team_name == best_submission.actor.name:
-                    holdout_execution_submissions[actor_email] = submission
+                    holdout_execution_submissions[actor_email] = best_submission
                 elif execute_team_name is None and best_loss < min_loss_criteria:
-                    holdout_execution_submissions[actor_email] = submission
+                    holdout_execution_submissions[actor_email] = best_submission
 
         return holdout_execution_submissions
 
