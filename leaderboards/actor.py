e67d453f3796032ce1d42bd46ac90651f7f40a29
Tim Blattner <timothy.blattner@nist.gov>
2022-09-14 12:10:47 -0400

    - Fixed issue storing uuid

--- leaderboards/actor.py
+++ leaderboards/actor.py
@@ -20,7 +20,7 @@ import uuid
 class Actor(object):
     VALID_TYPES = ['public', 'performer']
     def __init__(self, trojai_config: TrojaiConfig, email: str, name: str, poc_email: str, type: str, reset: bool = True):
-        self.uuid = uuid.uuid1()
+        self.uuid = str(uuid.uuid1())
         self.email = email
         self.name = name
         self.poc_email = poc_email
