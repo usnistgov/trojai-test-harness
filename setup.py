0e9341abfa4bcd25b88b13b702f180b44caee8f8
tjb3 <timothy.blattner@nist.gov>
2022-10-03 15:39:40 -0400

    - Updated URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='trojai-test-harness',
     version='1.0',
     packages=['data_science', 'data_science.one_off', 'leaderboards', 'local_evaluator'],
-    url='https://pages.nist.gov/trojai',
+    url='https://github.com/usnistgov/trojai-test-harness',
     license='',
     author='Tim Blattner',
     author_email='timothy.blattner@nist.gov',
