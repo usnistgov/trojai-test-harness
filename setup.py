ee58ee4fd624a0103860d55447ae9cdc44acb507
tjb3 <timothy.blattner@nist.gov>
2022-10-03 15:39:10 -0400

    - Added setup to formulate the test harness into a library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='trojai-test-harness',
+    version='1.0',
+    packages=['data_science', 'data_science.one_off', 'leaderboards', 'local_evaluator'],
+    url='https://pages.nist.gov/trojai',
+    license='',
+    author='Tim Blattner',
+    author_email='timothy.blattner@nist.gov',
+    description='Trojai Test Harness'
+)
