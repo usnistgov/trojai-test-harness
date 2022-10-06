1d31b68998e936e66666cca8d09e3ab113f590eb
tjb3 <timothy.blattner@nist.gov>
2022-10-06 10:58:45 -0400

    - Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,24 @@ setup(
     license='',
     author='Tim Blattner',
     author_email='timothy.blattner@nist.gov',
+    install_requires=[
+    'wheel',
+    'google-api-python-client',
+    'google-auth-httplib2',
+    'google-auth-oauthlib',
+    'jsonpickle',
+    'jsonschema',
+    'spython',
+    'hypothesis-jsonschema',
+    'pid',
+    'numpy',
+    'pytablewriter',
+    'dominate',
+    'GitPython',
+    'httplib2',
+    'sklearn',
+    'airium',
+    'pandas'
+    ],
     description='Trojai Test Harness'
 )
