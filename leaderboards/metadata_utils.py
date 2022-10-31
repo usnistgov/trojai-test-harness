b79d2463e48689147a2fae4a8b09a1e1192a3615
tjb3 <timothy.blattner@nist.gov>
2022-10-31 10:35:01 -0400

    - Changed names

--- leaderboards/metadata_utils.py
+++ leaderboards/metadata_utils.py
@@ -44,9 +44,9 @@ def build_model_lists(metadata_df: pd.DataFrame, columns_of_interest: list) -> d
         index = ''
         for key, value in permutation.items():
             if index == '':
-                index = '{}:{}'.format(key, value)
+                index = '{}={}'.format(key, value)
             else:
-                index += '_{}:{}'.format(key, value)
+                index += '_{}={}'.format(key, value)
             subset_df = subset_df[subset_df[key] == value]
 
         # Output the list of models that meet this requirement
