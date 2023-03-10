7dd855f69e4efe711d9d516f2fba4f13a8263280
Michael Majurski <mmajurski@gmail.com>
2023-03-09 22:52:47 -0500

    fixed python indenting as I was helped by pycharm

--- vm_scripts/evaluate_task.py
+++ vm_scripts/evaluate_task.py
@@ -14,17 +14,17 @@ from spython.main import Client
 from abc import ABC, abstractmethod
 
 class StreamToLogger(object):
-   """
-   Fake file-like stream object that redirects writes to a logger instance.
-   """
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
- 
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
 
     def flush(self):
         pass
