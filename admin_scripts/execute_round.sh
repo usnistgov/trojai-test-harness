4ffba18c848113077e1ed6cf41478853b9e09761
tjb3 <timothy.blattner@nist.gov>
2024-06-11 19:31:24 -0400

    Removed stream logging and updated execute round to go live

--- admin_scripts/execute_round.sh
+++ admin_scripts/execute_round.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-#PYTHONPATH=/mnt/isgnas/deploy/trojai/multi-round-leaderboard/trojai-test-harness /home/trojai/trojai-test-harness-env/bin/python3 -u /mnt/isgnas/deploy/trojai/multi-round-leaderboard/trojai-test-harness/leaderboards/check_and_launch_actors.py --trojai-config-filepath /mnt/isgnas/deploy/trojai/multi-round-leaderboard/trojai_config.json
+PYTHONPATH=/mnt/isgnas/deploy/trojai/multi-round-leaderboard/trojai-test-harness /home/trojai/trojai-test-harness-env/bin/python3 -u /mnt/isgnas/deploy/trojai/multi-round-leaderboard/trojai-test-harness/leaderboards/check_and_launch_actors.py --trojai-config-filepath /mnt/isgnas/deploy/trojai/multi-round-leaderboard/trojai_config.json
